f05ec2e4ba0c1a61a882b2adeaf5fcf055a2f518
fuyingren <fuyingren@songkegame.com>
2026-03-13 16:39:25 +0800

    优化

---
+++ backend/apps/ai_requirement/migrations/0008_merge_20260314.py
@@ -0,0 +1,14 @@
+# Generated merge: 解决 0007_remove_feature_breakdown 与 0007_remove_llmcalllog_... 的冲突
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ai_requirement', '0007_remove_feature_breakdown'),
+        ('ai_requirement', '0007_remove_llmcalllog_ai_req_llmcall_task_created_idx_and_more'),
+    ]
+
+    operations = [
+    ]
